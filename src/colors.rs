57ab0cbc39aa94073b4e6e98aac9d5f1b15bfef2
luyise <louise.leclerc37@gmail.com>
2021-06-11 16:21:20 +0200

    Splited main into 4 files, improved GUI and started a look command

---
+++ src/colors.rs
@@ -0,0 +1,6 @@
+use crossterm::style::Color;
+
+pub const BACKGROUND_CLR: Color = Color::Black;
+pub const PLAYER_CLR: Color = Color::White;
+pub const SCREEN_BOUNDARIES_CLR: Color = Color::AnsiValue(215);  // SandyBrown
+pub const GROUND_CLR: Color = Color::AnsiValue(242); // Grey42
--- src/main.rs
+++ src/main.rs
@@ -1,154 +1,18 @@
-// https://stackoverflow.com/questions/22273046/how-to-print-the-extended-ascii-code-in-java-from-integer-value/22274036
-// https://jonasjacek.github.io/colors/
+pub mod colors;
+pub mod game_structures;
+pub mod options;
+
+use colors::*;
+use game_structures::*;
+use options::*;
 
 use std::time::Duration;
 use std::io::stdout;
-use std::convert::TryInto;
 
 use crossterm::{event, cursor, terminal, execute, Result};
 use crossterm::event::{Event, KeyCode};
 use crossterm::terminal::ClearType;
 use crossterm::style::{Print, SetForegroundColor, SetBackgroundColor, Color};
-
-const BACKGROUND_CLR: Color = Color::Black;
-const PLAYER_CLR: Color = Color::White;
-const SCREEN_BOUNDARIES_CLR: Color = Color::AnsiValue(215);  // SandyBrown
-const GROUND_CLR: Color = Color::AnsiValue(242); // Grey42
-
-const SCREEN_WIDTH: u16 = 66;
-const SCREEN_HEIGHT: u16 = 26;
-const N_WIDTH: u16 = 64;
-const N_HEIGHT: u16 = 24;
-const MAP_WIDTH: u16 = 64;
-const MAP_HEIGHT: u16 = 24;
-
-struct Point {
-    x: u16,
-    y: u16
-}
-
-struct Player {
-    pos: Point,
-    sprite: char,
-}
-impl Player {
-    fn new() -> Player {
-        Player {
-            pos: Point { x: 0, y: 0 },
-            sprite: '@'
-        }   
-    }
-}
-
-struct NonPlayerCharacter {
-    pos: Point,
-    sprite: char,
-}
-
-struct Obstacle {
-    pos: Point,
-    sprite: char,
-}
-impl Obstacle {
-    fn new(i: u16, j: u16) -> Obstacle {
-        Obstacle {
-            pos: Point { x: i, y: j },
-            sprite: '\u{25A0}'
-        }   
-    }
-}
-
-struct Ground {
-    pos: Point
-}
-
-enum Entity {
-    NonPlayerCharacter(NonPlayerCharacter),
-    Obstacle(Obstacle),
-    Ground(Ground)
-}
-impl Entity {
-
-    fn get_char(&self) -> char {
-        match self {
-            Entity::NonPlayerCharacter(npc) => npc.sprite,
-            Entity::Obstacle(obs) => obs.sprite,
-            Entity::Ground(_) => '\u{2591}'
-        }
-    }
-
-    fn get_clr(&self) -> Color {
-        match self {
-            Entity::NonPlayerCharacter(_) => PLAYER_CLR,
-            Entity::Obstacle(_) => SCREEN_BOUNDARIES_CLR,
-            Entity::Ground(_) => GROUND_CLR
-        }
-    }
-
-    fn get_pos(&self) -> &Point {
-        match self {
-            Entity::NonPlayerCharacter(npc) => &npc.pos,
-            Entity::Obstacle(obs) => &obs.pos,
-            Entity::Ground(grd) => &grd.pos
-        }
-    }
-}
-
-#[derive(Clone)]
-struct ScreenState {
-    grid: [[(char, Color); N_HEIGHT as usize]; N_WIDTH as usize],
-}
-
-struct GameState {
-    player: Player,
-    entities: Vec<Entity>,
-}
-impl GameState {
-    fn new() -> GameState { 
-        GameState {
-            player: Player::new(),
-            entities: Vec::new()
-        }
-    }
-
-    fn make_screen_state(&self) -> ScreenState {
-
-        let mut grid = [[(' ', Color::Black); N_HEIGHT as usize]; N_WIDTH as usize];
-
-        for entity in &self.entities {
-            let pos = entity.get_pos();
-            // Ici on peut ajouter une condition pour tester si le point est dans l'écran ou non
-            grid[pos.x as usize][pos.y as usize] = (entity.get_char(), entity.get_clr())
-        };
-        grid[self.player.pos.x as usize][self.player.pos.y as usize] = ('@', PLAYER_CLR);
-
-        ScreenState { grid: grid }
-    }
-
-    fn move_player(&mut self, dx: i16, dy: i16) {
-        let x = self.player.pos.x;
-        let y = self.player.pos.y;
-        let nx: u16 = (x as i16 + dx).try_into().unwrap();
-        let ny: u16 = (y as i16 + dy).try_into().unwrap();
-
-        // Plus tard, il faudra remplcer ça par du scrolling
-        if nx >= 0 && nx < N_WIDTH && ny >= 0 && ny < N_HEIGHT {
-            
-            for e in self.entities.iter() {
-                let e_pos = e.get_pos();
-                let e_x = e_pos.x;
-                let e_y = e_pos.y;
-                if e_x == nx && e_y == ny {
-                    return ()
-                }
-            }
-            self.player.pos.x = nx;
-            self.player.pos.y = ny
-            
-        }
-
-    }
-}
 
 fn main() -> Result<()> {
     
@@ -163,9 +27,11 @@ fn main() -> Result<()> {
 
     let (_cols, _rows) = terminal::size()?;
     execute!(stdout(),
-        cursor::Hide,
         SetBackgroundColor(BACKGROUND_CLR),
-        terminal::Clear(ClearType::All)
+        terminal::SetSize(SCREEN_WIDTH, SCREEN_HEIGHT),
+        terminal::Clear(ClearType::All),
+        cursor::Hide,
+        terminal::SetTitle("Roguelike")
     )?;
     print_screen_background()?;
     print_screen(&screen_state)?;
@@ -180,7 +46,7 @@ fn main() -> Result<()> {
             // function returns `true`
             match event::read().unwrap() {
                 Event::Key(event) => match event.code {
-                    KeyCode::Left | KeyCode::Char('q') => {
+                    KeyCode::Left | KeyCode::Char('q') => {         // Deplacement
                         gs.move_player(-1, 0)
                     },
                     KeyCode::Right | KeyCode::Char('d') => {
@@ -192,7 +58,10 @@ fn main() -> Result<()> {
                     KeyCode::Down | KeyCode::Char('s') => {
                         gs.move_player(0, 1)
                     },
-                    KeyCode::Esc => { 
+                    KeyCode::Char('l') => {                         // Look command : [l]
+                        look()?
+                    }
+                    KeyCode::Esc => {                               // Exit command : [esc]
                         execute!(stdout(),
                             cursor::Show,
                             terminal::Clear(ClearType::All)
@@ -202,25 +71,34 @@ fn main() -> Result<()> {
                 },
                 _ => {}
             }
+            // while event::poll(Duration::from_millis(0)).unwrap() {
+            //     event::read();
+            // }
         } else {
             // Timeout expired and no `Event` is available
         };
 
         // II. Update
         screen_state = gs.make_screen_state();
-        
 
         // III. Render
         refresh_screen(old_screen_state, &screen_state)?;
         execute!(stdout(),
-            cursor::MoveTo(0, SCREEN_HEIGHT),
             SetForegroundColor(Color::White),
-            Print("This is an information!")
+            cursor::MoveTo(1, N_HEIGHT+2),
         )?;
-        // println!("{} {}", gs.player.pos.x, gs.player.pos.y);
+        println!("This is an information! ");
+        execute!(stdout(),
+            cursor::MoveTo(1, N_HEIGHT+3),
+        )?;
+        println!("DEBUG: player.pos : {} {}      ", gs.player.pos.x, gs.player.pos.y);
+        execute!(stdout(),
+            cursor::MoveTo(1, N_HEIGHT+4),
+        )?;
+        println!("DEBUG: screen.pos : {} {}      ", gs.screen_pos.x, gs.screen_pos.y);
 
         // IV. Time management
-        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60))
+        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30))
 
     };
     #[allow(unreachable_code)]
@@ -247,7 +125,7 @@ fn print_screen(screen_state : &ScreenState) -> Result<()> {
 fn refresh_screen(screen_state_already_displayed: ScreenState, screen_state_to_display : &ScreenState) -> Result<()> {
     for j in 0..N_HEIGHT {
         for i in 0..N_WIDTH {
-            if screen_state_already_displayed.grid[i as usize][j as usize].0 != screen_state_to_display.grid[i as usize][j as usize].0 {
+            if screen_state_already_displayed.grid[i as usize][j as usize] != screen_state_to_display.grid[i as usize][j as usize] {
                 disp(screen_state_to_display.grid[i as usize][j as usize].0, i, j, screen_state_to_display.grid[i as usize][j as usize].1)?
             }
         }
@@ -255,35 +133,108 @@ fn refresh_screen(screen_state_already_displayed: ScreenState, screen_state_to_d
     Ok (())
 }
 
+fn clean_environment() -> Result<()> {
+
+    execute!(stdout(), SetForegroundColor(GROUND_CLR))?;
+    let empty_strip: String = " ".repeat(N_HEIGHT as usize);
+    for j in 1..(SCREEN_HEIGHT-1) {
+        execute!(stdout(), cursor::MoveTo(N_WIDTH+2, j))?;
+        execute!(stdout(),
+            Print(&empty_strip)
+        )?  
+    };
+    Ok(())
+}
+
+fn look() -> Result<()> {
+
+    clean_environment()?;
+    execute!(stdout(),
+        cursor::MoveTo(N_WIDTH+2+10,1+4),
+        SetForegroundColor(Color::White),
+        Print('\u{250C}'.to_string())
+    )?;
+
+    Ok(())
+}
+
 fn print_screen_background() -> Result<()> {
 
     // Making Screen Boundaries
-    execute!(stdout(),
+    execute!(stdout(),                                  // Coin Haut-gauche
         cursor::MoveTo(0, 0),
         SetForegroundColor(SCREEN_BOUNDARIES_CLR),
         Print('\u{2554}'.to_string()),
     )?;
-    for _ in 1..(SCREEN_WIDTH-1) {
+    for _ in 1..=(N_WIDTH) {                            // Bord supérieur
         execute!(stdout(),
             Print('\u{2550}'.to_string()),
         )?
     };
     execute!(stdout(),
-            Print('\u{2557}'.to_string()),
+            Print('\u{2566}'.to_string()),
     )?;
-    for j in 1..(SCREEN_HEIGHT-1) {
+    for _ in (N_WIDTH+2)..(SCREEN_WIDTH-1) {                
+        execute!(stdout(),
+            Print('\u{2550}'.to_string()),
+        )?
+    };
+    execute!(stdout(),
+        Print('\u{2557}'.to_string()),
+    )?;
+    for j in 1..=(N_HEIGHT) {                              // Bordures verticales
         execute!(stdout(),
             cursor::MoveTo(0, j),
+            Print('\u{2551}'.to_string()),
+            cursor::MoveTo(N_WIDTH+1, j),
             Print('\u{2551}'.to_string()),
             cursor::MoveTo(SCREEN_WIDTH-1, j),
             Print('\u{2551}'.to_string()),
         )?
     };
-    execute!(stdout(),
-        cursor::MoveTo(0, SCREEN_HEIGHT-1),
-        Print('\u{255A}'.to_string()),
+    execute!(stdout(),                                      
+        cursor::MoveTo(0, N_HEIGHT+1),
+        Print('\u{2560}'.to_string()),
     )?;
-    for _ in 1..(SCREEN_WIDTH-1) {
+    execute!(stdout(),
+        cursor::MoveTo(N_WIDTH+1, N_HEIGHT+1),
+        Print('\u{2563}'.to_string()),
+    )?;
+    execute!(stdout(),
+        cursor::MoveTo(SCREEN_WIDTH-1, N_HEIGHT+1),
+        Print('\u{2551}'.to_string()),
+    )?;
+    for j in (N_HEIGHT+2)..(SCREEN_HEIGHT-1) {             
+        execute!(stdout(),
+            cursor::MoveTo(0, j),
+            Print('\u{2551}'.to_string()),
+            cursor::MoveTo(N_WIDTH+1, j),
+            Print('\u{2551}'.to_string()),
+            cursor::MoveTo(SCREEN_WIDTH-1, j),
+            Print('\u{2551}'.to_string()),
+        )?
+    };
+    execute!(stdout(),                          // Bord horizontal intermédiaire
+        cursor::MoveTo(1, N_HEIGHT+1)
+    )?;
+    for _ in 1..=(N_WIDTH) {                                
+        execute!(stdout(),
+            Print('\u{2550}'.to_string()),
+        )?
+    };
+    execute!(stdout(),                          // Bord horizontal inférieure
+        cursor::MoveTo(0, SCREEN_HEIGHT-1),
+        Print('\u{255A}'.to_string())
+    )?;
+    for _ in 1..=(N_WIDTH) {                                
+        execute!(stdout(),
+            Print('\u{2550}'.to_string()),
+        )?
+    };
+    execute!(stdout(),
+        Print('\u{2569}'.to_string())
+    )?;
+    for _ in (N_WIDTH+2)..(SCREEN_WIDTH-1) {
         execute!(stdout(),
             Print('\u{2550}'.to_string()),
         )?
@@ -291,16 +242,6 @@ fn print_screen_background() -> Result<()> {
     execute!(stdout(),
         Print('\u{255D}'.to_string()),
     )?;
-
-    // // Making Ground
-    // execute!(stdout(), SetForegroundColor(GROUND_CLR))?;
-    // let ground_strip: String = "\u{2591}".repeat((SCREEN_WIDTH-2) as usize);
-    // for j in 1..SCREEN_HEIGHT-1 {
-    //     execute!(stdout(), cursor::MoveTo(1, j))?;
-    //     execute!(stdout(),
-    //         Print(&ground_strip)
-    //     )?  
-    // };
 
     Ok(())
 }
