314d5e7f816045c2fab514ac5bc88a4ffae0d479
Samuel VIVIEN <samuel.aime.vivien@free.fr>
2021-06-13 22:23:32 +0200

    implemented as first shot for saving

--- src/game_structures/player.rs
+++ src/game_structures/player.rs
@@ -1,4 +1,6 @@
 use super::Point;
+use std::fs::File;
+use std::io::Write;
 
 pub struct Player {
     pub pos: Point,
@@ -10,5 +12,14 @@ impl Player {
             pos: Point { x: 4, y: 5 },
             sprite: '@',
         }
+    }
+
+    pub fn save(self, f: &mut File) -> std::io::Result<usize> {
+        f.write(b"Player {\n\t pos:\n")?;
+        self.pos.save(f)?;
+        let mut s = String::from("\t sprite: '");
+        s.push(self.sprite);
+        s.push_str("'\n}\n");
+        f.write(s.as_bytes())
     }
 }
